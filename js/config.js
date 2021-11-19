39a90041f76ac70629764b4ecb02614a7cedaf9c
89VAnh <89075517+89VAnh@users.noreply.github.com>
2021-11-19 15:18:27 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+  titleWeb: "Gửi crush của tớ",
+  introTitle: "Crush yêu dấu",
+  introDesc: "Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha",
+  btnIntro: "^^HiHi^^",
+  title: "Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 💖",
+  desc: "Thoát web là chấp nhận yêu tớ rồi nhé!!!",
+  btnYes: "Thích lắm <33",
+  btnNo: "Không nha :3",
+  question: "Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình 💖",
+  btnReply: "Gửi cho bạn <3",
+  reply: "Yêu thì yêu mà không yêu thì yêu <33333333",
+  mess: "Mình biết mà 😍. Yêu bạn nhiều nhiều 😘😘",
+  messDesc: "Tối nay, mình qua đón đi chơi nha.",
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://m.me", //link mess của các bạn. VD: /dinhkaito
+};
